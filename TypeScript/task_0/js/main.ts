ebaac790d3432eeb253f9d32a61c9494e1baa4cb
PakizaMutallimova <pakizemutellimova97@mail.ru>
2024-09-28 19:06:18 +0400

    added

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -1,33 +1,49 @@
 interface Student {
-    firstName: string
-    lastName: string
-    age: number
-    location: string
-}
-
-const student1: Student = {
-    firstName: "Helen",
-    lastName: "Doe",
-    age: 19,
-    location: "London"
-}
-
-const student2: Student = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 27,
-    location: "Texas"
-}
-
-
-const studentsList: Student[] = [student1, student2];
-const table: HTMLTableElement = document.createElement('table');
-document.body.appendChild(table);
-
-studentsList.forEach((student: Student): void => {
-	const newRow: HTMLTableRowElement = table.insertRow();
-	const newRowFirstName: HTMLTableCellElement = newRow.insertCell();
-	const newRowLocation: HTMLTableCellElement = newRow.insertCell();
-	newRowFirstName.innerHTML = student.firstName;
-	newRowLocation.innerHTML = student.location;
-})
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+  }
+  
+  const student1: Student = {
+    firstName: "Larry",
+    lastName: "Hudson",
+    age: 32,
+    location: "San Francisco",
+  };
+  
+  const student2: Student = {
+    firstName: "Karla",
+    lastName: "Dermond",
+    age: 26,
+    location: "New York",
+  };
+  
+  const studentsList: Array<Student> = [student1, student2];
+  
+  const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+  
+  const table: HTMLTableElement = document.createElement("table");
+  const thead: HTMLTableSectionElement = document.createElement("thead");
+  const tbody: HTMLTableSectionElement = document.createElement("tbody");
+  
+  const rowHead: HTMLTableRowElement = thead.insertRow(0);
+  const cell1Head: HTMLTableCellElement = rowHead.insertCell(0);
+  const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
+  
+  cell1Head.innerHTML = "firstName";
+  cell2Head.innerHTML = "location";
+  
+  table.append(thead);
+  
+  studentsList.forEach((student) => {
+    const row: HTMLTableRowElement = tbody.insertRow(0);
+    const cell1: HTMLTableCellElement = row.insertCell(0);
+    const cell2: HTMLTableCellElement = row.insertCell(1);
+  
+    cell1.innerHTML = student.firstName;
+    cell2.innerHTML = student.location;
+  });
+  
+  table.append(tbody);
+  body.append(table);
--- TypeScript/task_0/webpack.config.js
+++ TypeScript/task_0/webpack.config.js
@@ -1,32 +1,37 @@
-
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
-  entry: './task_0/js/main.ts', // Adjust this path to where your main.ts is located
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+  entry: "./js/main.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
       },
     ],
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
   plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './task_0/index.html', // Adjust the path to your HTML file
+      title: "Development",
     }),
   ],
-  mode: 'development', // Set 'development' or 'production'
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
